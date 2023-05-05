2c2d8709da11981620b7483dffe927f32d55d2a9
otavio24R <otaviorodrigues_gmr@hotmail.com>
2023-05-05 09:33:36 +0100

    criamos o projeto

---
+++ Exercicio - Banco/Conta_Bancaria.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio___Banco
+{
+    internal class Conta_Bancaria
+    {
+        //Definição de propriedades
+
+        public int Numero { get; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }  //saldo //nao é disponivel para ser modificado o saldo (private set)
+
+
+
+
+        // Construtores
+
+        public Conta_Bancaria(int numero, string titular)   // este construtor permite instanciar contas bancarias apenas com o numero de conta e seu o seu titular.
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public Conta_Bancaria(int numero, string titular, double saldo) : this(numero, titular)   // permite instanciar uma conta a partir do seu nº de conta, tittular e seu saldo inicial.
+        {
+            Saldo = saldo;
+        }
+
+
+
+
+        // secção de métodos
+
+        public void Deposito(double quantia)      // Este é o metodo que permite depositar o dinheiro na conta
+        {
+
+            Saldo += quantia;
+        }
+
+
+        public void Levantamento(double quantia) // Este metodo permite fazer o levantamento de dinheiro
+        {
+
+            Saldo -= quantia + 5;   // podemos acrescentar a taxa de 5
+        }
+
+        public override string ToString()
+        {
+            return "Conta bancaria numero "
+            + Numero
+            +", Titular da conta: "
+            + Titular
+            + ", Saldo da conta: "
+            + Saldo.ToString("F2")
+            + " euros.";
+        }
+
+
+
+
+    }
+}
---
+++ Exercicio - Banco/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Exercicio___Banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta_Bancaria cb;
+
+            Console.WriteLine("Entre com nº de conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nome do titular: ");
+            string titular = Console.ReadLine();
+            Console.WriteLine("Deseja fazer um depósito inicial?");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S')
+            {
+                Console.WriteLine("Qual é o valor do depósito inicial?");
+                double depositoInicial = double.Parse(Console.ReadLine());
+                cb = new Conta_Bancaria(numero, titular, depositoInicial);
+
+
+            }
+            else { cb = new Conta_Bancaria(numero, titular); }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(cb);
+            Console.WriteLine();
+            
+            Console.WriteLine("Valor a depositar: ");
+            double quantia = double.Parse(Console.ReadLine());
+            cb.Deposito(quantia);
+            Console.WriteLine("Dados atualizados da conta: ");
+            Console.WriteLine(cb);
+
+            Console.WriteLine();
+            Console.WriteLine("Que valor pretende levantar?");
+            cb.Levantamento(quantia);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados da conta: ");
+            Console.WriteLine(cb);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
